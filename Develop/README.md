528660e3219b2036c737f6ad44f9cb03198b7bf1
joel cronin <you@example.com>
2022-06-09 17:00:46 -0400

    Questions to be asked written out

---
+++ Develop/README.md
@@ -0,0 +1,57 @@
+
+
+
+  
+  
+# Title
+
+  ![License](https://img.shields.io/badge/license-Mozilla-green)
+
+
+  ## Description
+
+  I did
+
+  ## Table of Contents
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Contributing](#contributing)
+
+  [Tests](#tests)
+
+  [License](#license)
+
+  [Questions](#questions)
+
+  ## Installation
+
+  I app
+
+  ## Usage
+
+  I use
+
+  ## contributing
+
+  I cont
+
+  ## Tests
+
+  I test
+
+  ## License
+
+  This application is covered under the following license: Mozilla Public License 2.0
+
+  ## Questions
+
+   You can view more information here: gg
+
+  
+
+  If you have any further questions please them to: 123@
+
+  
---
+++ Develop/utils/questions.js
@@ -0,0 +1,50 @@
+const questions = [
+    {
+        name: "title",
+        message: "What is the title of this README?",
+        type: "input"
+      },
+      {
+        name: "description",
+        message: "How would you summarise what you did in this project?",
+        type: "input" 
+      },
+      {
+        name: "installation",
+        message: "What do users have to do to install this application?",
+        type: "input" 
+      },
+      {
+        name: "Usage",
+        message: "How do users use this application?",
+        type: "input" 
+      }, 
+      {
+        name: "contributing",
+        message: "How can others contribute to this application?",
+        type: "input" 
+      }, 
+      {
+        name: "Tests",
+        message: "Are there any tests users whould run?",
+        type: "input" 
+      }, 
+      {
+        name: "email",
+        message: "What is your email address?",
+        type: "input" 
+      }, 
+      {
+        name: "GitHub",
+        message: "What is your GitHub usename?",
+        type: "input" 
+      }, 
+      {
+        name: "licences",
+        message: "What License is this covered by?",
+        type: "list",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"]
+        },
+];
+
+module.exports = questions;
